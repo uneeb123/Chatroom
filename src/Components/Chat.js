382ec01632ed411a6742e7e293f7a28b990031be
Uneeb Agha <uneeb.agha@gmail.com>
2018-10-20 18:53:54 -0400

    added chat buttom

--- src/Components/Chat.js
+++ src/Components/Chat.js
@@ -8,7 +8,14 @@ export default class Chat extends Component {
         <div className="row h-100 no-gutters">
           <div className="Chat-box col-md-9 h-100">
             <div className="Chat-box-display" />
-            <div className="Chat-box-message" />
+            <div className="Chat-box-message container-fluid">
+              <div className="row h-100">
+                <div className="col-md-10"/>
+                <div className="Chat-box-message-btn col-md-2">
+                  <i className="fas fa-comment fa-3x"></i>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="Chat-friends col-md-3" />
         </div>
